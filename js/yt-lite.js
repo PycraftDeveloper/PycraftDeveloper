2dca349386a5519db4dfffa05c12a2a701c182d5
Thomas Jebson <ThomasJebbo@gmail.com>
2026-08-03 18:54:31 +0100

    Work on improving portfolio

--- js/yt-lite.js
+++ js/yt-lite.js
@@ -1,9 +1,33 @@
+const thumbnails = [
+    "maxresdefault.jpg",
+    "sddefault.jpg",
+    "hqdefault.jpg"
+];
+
+function setBestThumbnail(el, id, index = 0) {
+    if (index >= thumbnails.length) return;
+
+    const url = `https://i.ytimg.com/vi/${id}/${thumbnails[index]}`;
+    const img = new Image();
+
+    img.onload = () => {
+        if (img.naturalWidth > 120) {
+            el.style.backgroundImage = `url(${url})`;
+        } else {
+            setBestThumbnail(el, id, index + 1);
+        }
+    };
+
+    img.onerror = () => setBestThumbnail(el, id, index + 1);
+
+    img.src = url;
+}
+
 document.querySelectorAll(".yt-lite").forEach(el => {
     const id = el.dataset.id;
 
     // load thumbnail (VERY fast, no iframe yet)
-    el.style.backgroundImage =
-        `url(https://i.ytimg.com/vi/${id}/hqdefault.jpg)`;
+    setBestThumbnail(el, id);
 
     el.addEventListener("click", () => {
         const iframe = document.createElement("iframe");
@@ -11,8 +35,11 @@ document.querySelectorAll(".yt-lite").forEach(el => {
         iframe.allow =
             "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
         iframe.allowFullscreen = true;
+        iframe.style.display = "block";
         iframe.style.width = "100%";
+        iframe.style.height = "100%";
         iframe.style.aspectRatio = "16 / 9";
+        iframe.style.border = "0";
 
         el.replaceWith(iframe);
     });
